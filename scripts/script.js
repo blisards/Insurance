995a5c870196e5715298f2c8b1f378bd623c2fee
blisards <blisard.samuel@gmail.com>
2024-12-27 16:01:46 -0800

    website

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("quote-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you! Your quote request has been submitted.");
+  });
+  
